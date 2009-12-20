c9bd068b661009e51ed29fd47efdf5a521d56991
Tadashi MATSUO <tad@mymail.twin.jp>
2009-12-20 18:54:34 +0900

    The variable 'twittering-timeline-history' is added.

--- twittering-mode.el
+++ twittering-mode.el
@@ -79,6 +79,7 @@ stored here. DO NOT SET VALUE MANUALLY.")
 
 (defvar twittering-tweet-history nil)
 (defvar twittering-user-history nil)
+(defvar twittering-timeline-history nil)
 (defvar twittering-hashtag-history nil)
 
 (defvar twittering-number-of-tweets-on-retrieval 20
@@ -237,6 +238,13 @@ SSL connections use 'curl' command as a backend.")
 	,(concat "1/" username "/lists/" list-name "/statuses" ))))
    (t
     `("twitter.com" ,(concat "statuses/user_timeline/" timeline-spec)))))
+
+(defun twittering-add-timeline-history (&optional timeline-spec)
+  (let ((timeline-spec (or timeline-spec (twittering-get-timeline-spec))))
+    (when
+	(or (null twittering-timeline-history)
+	    (not (string= timeline-spec (car twittering-timeline-history))))
+      (add-to-history 'twittering-timeline-history timeline-spec))))
 
 (defun assocref (item alist)
   (cdr (assoc item alist)))
@@ -910,6 +918,7 @@ Available keywords:
 			 noninteractive)
 		    (run-hooks 'twittering-new-tweets-hook))
 		(twittering-render-timeline)
+		(twittering-add-timeline-history)
 		(when twittering-notify-successful-http-get
 		  (message (if suc-msg suc-msg "Success: Get."))))
 	       (t (message status))))
